60b4111f51af61b64755a31afc57062ee724ac00
Arnold Z <arnold.h.zhang@gmail.com>
2024-02-14 10:48:26 -0600

    Update main.py
    - Found the HTML div element that contains the price for each item
    - websiteHTML() function prints the span element containing the price

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from bs4 import BeautifulSoup
 # Function that retrieves the HTML code for a certain website using the requests library
 # Also formats the retrieved HTML code using the prettify() function from the BeautifulSoup library
 def websiteHTML():
-    r = requests.get('https://stockx.com/nike-kyrie-5-spongebob-pineapple-house')
+    r = requests.get('https://www.justballgloves.com/product/mizuno-mvp-prime-11-5--baseball-glove--gmvp1151p4/34520/#attr=255076')
     website_content = r.content
     
     soup = BeautifulSoup(r.content, 'html.parser')  
-    return soup.prettify()
+    
+    s = soup.find('div', class_="price")
+    content = s.find_all('span')
+
+    print(content)
+
+websiteHTML()
