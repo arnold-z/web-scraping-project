5daadabf94aac6b70df1a428f3bcf2f6ab56aaa4
arnold-z <arnold.h.zhang@gmail.com>
2024-02-25 18:46:47 -0600

    Added more system arguments for user functionality Removed unnecessary HTML code from price output

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-link = sys.argv[1]
+# Stores the user's desired website link
+search_query = sys.argv[1]
+link = sys.argv[2]
 
-# Function that retrieves the HTML code for a certain website using the requests library
-# Also formats the retrieved HTML code using the prettify() function from the BeautifulSoup library
-def websiteHTML():
+# Retrieves the HTML code for a certain website using the requests library
+# Formats the retrieved HTML code using the BeautifulSoup library
+def priceScraper():
     r = requests.get(link)
     website_content = r.content
-    
     soup = BeautifulSoup(r.content, 'html.parser')  
-    
     s = soup.find('div', class_="price")
-    content = s.find_all('span')
+    content = s.find_all('span')    
 
-    print(content)
+    price_list = []
 
-websiteHTML()
+    for c in content:
+       price_list.append(c.text)
+   
+    print(price_list[0])
+
+if search_query == "-price":
+    priceScraper()
